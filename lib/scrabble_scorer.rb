3fb7f846b2f6a7dfd9db458966b6ddf40ef74d1c
Chris Ward <chriswarddev@gmail.com>
2015-02-20 13:28:29 +0000

    Ed and Chris

---
+++ lib/scrabble_scorer.rb
@@ -0,0 +1,14 @@
+class ScrabbleScorer
+
+  def input_word(input_string)
+    @input_string = input_string
+  end
+
+  def convert(input_string)
+    input_string.chars
+  end
+
+  def import(text_file)
+    text_file
+  end
+end
--- spec/scrabble_scorer_spec.rb
+++ spec/scrabble_scorer_spec.rb
@@ -1,1 +1,25 @@
 require 'scrabble_scorer'
+
+describe ScrabbleScorer do
+
+  scorer = ScrabbleScorer.new
+  file = ["AA", "AAH", "AAHED", "AAHING", "AAHS", "AAL", "AALII", "AALIIS", "AALS", "AARDVARK"]
+
+  it "should be able to accept a string" do
+    expect(scorer.input_word("string")).to eq "string"
+  end
+
+  it 'should create a convert a string into an array' do
+    expect(scorer.convert("str")).to eq ["s","t","r"]
+  end
+
+  it 'should be able to import the word list' do
+    expect(scorer.import(file)).to be_an Array
+  end
+
+  it 'should select all the words that match the input string' do
+    expect
+  end
+
+
+end
